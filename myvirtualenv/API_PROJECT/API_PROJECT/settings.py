0a8161dee0e2f723a25c2bb38492a65856080726
Saiful Islam Sagar <46032500+sisagarbd@users.noreply.github.com>
2023-09-04 13:47:04 +0600

    das

--- myvirtualenv/API_PROJECT/API_PROJECT/settings.py
+++ myvirtualenv/API_PROJECT/API_PROJECT/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# TEMPLATES_DIR = Path(__file__).resolve().parent 
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'API_PROJECT.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
